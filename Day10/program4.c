d6cbfe6c3915d86ab71c1d16e04a9095d6478499
Saurabh Singh <saurabhpratapsingh643@gmail.com>
2026-07-08 12:58:15 +0530

    Tenth assignment

---
+++ Day10/program4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, rows;
+
+    printf("Enter rows: ");
+    scanf("%d", &rows);
+
+    for (i = 1; i <= rows; i++) {
+        for (j = 1; j <= rows - i; j++) {
+            printf(" ");
+        }
+
+        for (j = 1; j <= i; j++) {
+            printf("%c", j+64);
+        }
+
+        for (j = i - 1; j >= 1; j--) {
+            printf("%c", j+64);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
